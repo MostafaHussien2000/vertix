e86e744bede3d83b7daa5649a25ffd088efa3d23
MostafaHussien2000 <mabozeed70@gmail.com>
2024-06-04 00:15:26 +0300

    fetching data

--- src/api/api.js
+++ src/api/api.js
@@ -2,4 +2,10 @@ const API_URL = process.env.REACT_APP_TMDB_API_URL;
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 export const popularMoviesURL = (adult = false, video = false, page = 2) =>
-  `${API_URL}/movie?include_adult=${adult}&include_video=${video}&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`;
+  `${API_URL}/3/discover/movie?include_adult=${adult}&include_video=${video}&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`;
+
+export const nowPlayingMoviesURL = (adult = false, video = false, page = 1) =>
+  `${API_URL}/3/movie/now_playing?language=en-US&page=1&api_key=${API_KEY}`;
+
+export const getImagesURL = (size = "w500") =>
+  `https://image.tmdb.org/t/p/${size}`;
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,26 +1,38 @@
 import React, { useEffect, useState } from "react";
 
 import logo from "../static/logo.png";
-import { useFetch } from "../hooks/useFetch";
-import { popularMoviesURL } from "../api/api";
+import {
+  getImagesURL,
+  nowPlayingMoviesURL,
+  popularMoviesURL,
+} from "../api/api";
+import {
+  TbHome,
+  TbLogout,
+  TbSettings2,
+  TbStarFilled,
+  TbVideo,
+} from "react-icons/tb";
+import Loader from "../components/Loader";
 
 function FeedPage() {
-  const [movies, setMovies] = useState([]);
+  const NUMBER_OF_FEATURED_MOVIES = 3;
+  const [movies, setMovies] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch(popularMoviesURL())
+    fetch(nowPlayingMoviesURL())
       .then((response) => response.json())
       .then((data) => {
-        setMovies(data.results);
+        setMovies((m) => ({ ...m, nowPlaying: data.results }));
         setLoading(false);
         setError(false);
       })
       .catch((err) => {
-        throw new Error(err);
         setLoading(false);
         setError(true);
+        throw new Error(err);
       });
   }, []);
 
@@ -30,6 +42,7 @@ function FeedPage() {
     profilePic:
       "https://th.bing.com/th/id/OIP.4A2g1d0ruPQwtDlpBl5OuQHaHZ?rs=1&pid=ImgDetMain",
   };
+
   return (
     <main className="feed-page">
       <aside className="feed-page__side-nav">
@@ -38,11 +51,11 @@ function FeedPage() {
           <h3>Menu</h3>
           <ul>
             <li>
-              // icon
+              <TbHome />
               <span>Home</span>
             </li>
             <li>
-              // icon
+              <TbVideo />
               <span>Coming Soon</span>
             </li>
           </ul>
@@ -50,29 +63,68 @@ function FeedPage() {
         <div className="feed-page__side-nav__options">
           <ul>
             <li>
-              // icon
+              <TbSettings2 />
               <span>Settings</span>
             </li>
             <li>
-              // icon
+              <TbLogout />
               <span>Logout</span>
             </li>
           </ul>
         </div>
       </aside>
-      <hr />
       <section className="feed-page__main-content">
-        <div className="feed-page__main-content__featured">
-          {movies ? (
-            <ul>
-              {movies.map((movie) => (
-                <li key={movie.id}>{movie.original_title}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>No data found</p>
-          )}
-        </div>
+        {loading ? (
+          <center>
+            <Loader />
+          </center>
+        ) : error ? (
+          <p>something went wrong</p>
+        ) : movies ? (
+          <>
+            <div className="feed-page__main-content__featured">
+              {movies?.nowPlaying
+                ?.slice(0, NUMBER_OF_FEATURED_MOVIES)
+                .map((movie) => (
+                  <li key={movie.id}>
+                    <img src={`${getImagesURL()}/${movie.backdrop_path}`} />
+                    <span>{movie.original_title}</span>
+                  </li>
+                ))}
+            </div>
+            <div className="feed-page__main-content__popular">
+              <h1>Popular Movies</h1>
+              <ul>
+                {movies?.nowPlaying
+                  ?.slice(
+                    NUMBER_OF_FEATURED_MOVIES,
+                    NUMBER_OF_FEATURED_MOVIES + 4
+                  )
+                  .map((movie) => (
+                    <li
+                      className="feed-page__main-content__popular__movie-card"
+                      key={movie.id}
+                    >
+                      <img
+                        className="feed-page__main-content__popular__movie-card__poster"
+                        src={`${getImagesURL()}/${movie.poster_path}`}
+                      />
+                      <h5 className="feed-page__main-content__popular__movie-card__name">
+                        {movie.original_title}
+                      </h5>
+                      <p className="feed-page__main-content__popular__movie-card__rate">
+                        <TbStarFilled />
+
+                        <span>{movie.vote_average}</span>
+                      </p>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </>
+        ) : (
+          <p>No data found</p>
+        )}
       </section>
       <aside className="feed-page__side-content"></aside>
     </main>
